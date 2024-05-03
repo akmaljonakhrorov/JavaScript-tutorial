e138a678d5e50f6308782fee99980370c658ccb5
akmaljonakhrorov <117590078+akmaljonakhrorov@users.noreply.github.com>
2024-05-03 10:51:27 +0500

    Add files via upload

---
+++ taskrecursion.js
@@ -0,0 +1,61 @@
+/* 1 - task,  100 gacha bo'lgan sonlar yig'indisini hisoblovchi funcksiya yozing */
+
+
+
+/* function sumTo(n) {
+    let total = 0;
+    for (let i = 1; i <= n; i++) {
+        total += i;
+    }
+    return total;
+}
+console.log(sumTo(1));  // Output: 1
+console.log(sumTo(2));  // Output: 3
+console.log(sumTo(3));  // Output: 6
+console.log(sumTo(4));  // Output: 10
+console.log(sumTo(100));// Output: 5050 */
+
+
+/*function sumTo(n){
+    if(n===1) {
+        return 1;
+    } else {
+        return n + sumTo(n-1);
+    }
+}
+
+console.log(sumTo(1));
+console.log(sumTo(2));
+console.log(sumTo(3));
+console.log(sumTo(4));
+console.log(sumTo(100));
+*/
+
+/* TASK 2, 1 dan 5 gacha bo'lgan sonlarni factorialini toping 
+function factorNumber(n) {
+    if(n==0 || n==1){
+        return 1;
+    } else {
+        return n * factorNumber(n-1)
+    }
+}
+console.log(factorNumber(1));
+console.log(factorNumber(2));
+console.log(factorNumber(3));
+console.log(factorNumber(4));
+console.log(factorNumber(5)); 
+*/
+
+/* TASK 3, fibobacci number yigindisini toping:  */
+/* function fibonacciFunc(n) {
+    if(n == 1 || n == 2) {
+        return 1;
+    } else {
+        return fibonacciFunc(n-1) + fibonacciFunc(n-2)
+    }
+}
+
+console.log(fibonacciFunc(1));
+console.log(fibonacciFunc(3));
+console.log(fibonacciFunc(7));
+console.log(fibonacciFunc(77)); */
